59421514b669065dddbd3c3d3b45e4ec5c462632
shanefitzpatrickk <shanefitz999@gmail.com>
2025-04-06 01:51:47 -0400

    Fix build: Move postcss-preset-env to dependencies and simplify config

--- portfolio_nextjs/postcss.config.js
+++ portfolio_nextjs/postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    'postcss-preset-env': {
-      features: {
-        'custom-properties': false
-      }
-    }
+    autoprefixer: {}
   }
 }; 
